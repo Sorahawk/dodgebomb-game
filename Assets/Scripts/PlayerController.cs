2505f78ac1e5eb54418b0c5dae2312387784c798
Sorahawk <sorahawktech@gmail.com>
2022-07-03 16:31:54 +0800

    added translation algorithm for dashing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,9 @@ public class PlayerController : MonoBehaviour {
     private Vector2 spinVal;
     private Vector3 latestDir;
 
+    private bool dashActivated = false;
+    private float dashDistance = 2;
+
     void OnMove(InputValue value) {
         moveVal = value.Get<Vector2>();
     }
@@ -19,12 +22,32 @@ public class PlayerController : MonoBehaviour {
         spinVal = value.Get<Vector2>();
     }
 
-    void Update() {
-        // apply translation to the world axes, so movement direction is constant and follows thumbsticks
-        Vector3 translation = new Vector3(moveVal.x, 0, moveVal.y) * moveSpeed * Time.deltaTime;
-        transform.Translate(translation, Space.World);
+    void OnDash() {
+        dashActivated = true;
+    }
 
-        // rotate object corresponding to spin input, by looking at world coordinates
+    void Update() {
+        // move
+        // apply translation to the world axes, so movement direction is constant and follows thumbsticks
+        Vector3 translation = new Vector3(moveVal.x, 0, moveVal.y);
+        transform.Translate(translation * moveSpeed * Time.deltaTime, Space.World);
+
+        // dash
+        if (dashActivated) {
+            dashActivated = false;
+
+            // if character is moving, apply dash in direction of movement
+            // otherwise if character is stationary, apply dash in direction that player is facing
+
+            if (translation != Vector3.zero) {
+                transform.Translate(translation * dashDistance, Space.World);
+            } else {
+                transform.Translate(Vector3.forward * dashDistance);
+            }
+        }
+
+        // spin
+        // rotate character according to spin input, by "looking at" corresponding world coordinates
         float lookX = transform.position.x + spinVal.x;
         float lookZ = transform.position.z + spinVal.y;
         transform.LookAt(new Vector3(lookX, 0, lookZ));
