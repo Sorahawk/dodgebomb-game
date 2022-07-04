382f3c32d5f64cb0c25199ca44f45932d86ce8a6
Sorahawk <sorahawktech@gmail.com>
2022-07-04 12:44:42 +0800

    implemented picking and dropping of bombs; created a simple bomb prefab with rigidbody settings tweaked

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,21 @@ using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour {
+    // move
     private float moveSpeed = 10;
     private Vector2 moveVal;
 
+    // spin
     private Vector2 spinVal;
     private Vector3 latestDir;
 
+    // dash
     private bool dashActivated = false;
     private float dashDistance = 2;
+
+    // bomb
+    public GameObject pickableBomb = null;
+    public GameObject carriedBomb = null;
 
     void OnMove(InputValue value) {
         moveVal = value.Get<Vector2>();
@@ -24,6 +31,43 @@ public class PlayerController : MonoBehaviour {
 
     void OnDash() {
         dashActivated = true;
+    }
+
+    void OnPickUpDrop() {
+        // check that not carrying any bombs, and a bomb is pickable
+        if (!carriedBomb && pickableBomb) {
+            // attach bomb to player
+            pickableBomb.transform.SetParent(gameObject.transform);
+            carriedBomb = pickableBomb;
+            pickableBomb = null;
+
+            // turn off bomb's useGravity so it stays with the player
+            carriedBomb.GetComponent<Rigidbody>().useGravity = false;
+        }
+
+        // if already carrying a bomb, drop it
+        else if (carriedBomb) {
+            // turn the bomb's useGravity back on
+            carriedBomb.GetComponent<Rigidbody>().useGravity = true;
+
+            // detach bomb
+            carriedBomb.transform.SetParent(null);
+            carriedBomb = null;
+        }
+    }
+
+    void OnThrow() {
+        // check if a bomb is carried
+        if (carriedBomb) {
+            // turn on bomb's useGravity
+            carriedBomb.GetComponent<Rigidbody>().useGravity = true;
+
+            // detach bomb from player
+            carriedBomb.transform.SetParent(null);
+            carriedBomb = null;
+
+            // TODO: throw bomb
+        }
     }
 
     void Update() {
@@ -53,5 +97,26 @@ public class PlayerController : MonoBehaviour {
         float lookZ = transform.position.z + spinVal.y;
 
         transform.LookAt(new Vector3(lookX, lookY, lookZ));
+    }
+
+    // use OnCollisionStay to reconfirm object collision every frame
+    void OnCollisionStay(Collision col) {
+        if (col.gameObject.CompareTag("Bomb")) {
+            System.String colliderName = col.GetContact(0).thisCollider.name;
+
+            // can only pick up if bomb is from the front
+            if (colliderName == "FrontCollider") {
+                pickableBomb = col.gameObject;
+
+                // TODO: check whether bomb is in-air or on the ground
+                // if in air, then pick up automatically if not already carrying a bomb
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision col) {
+        if (col.gameObject.CompareTag("Bomb")) {
+            pickableBomb = null;
+        }
     }
 }
