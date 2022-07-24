fb0ea514db66e52530a5fa64d2cf896a5febc89f
rebbyling <rebecca.lingzw@gmail.com>
2022-07-24 22:35:39 +0800

    lobby loads to demomap1

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject playButton;
+    public GameObject backButton;
+
+    public GameObject Canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadCanvas()
+    {
+        Canvas.SetActive(true);
+    }
+}
