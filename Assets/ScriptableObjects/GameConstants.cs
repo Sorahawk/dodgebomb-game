0a0eb4d585d8f0515f8b04bff0665afbf067678e
Yi Xian <yyx543@gmail.com>
2022-07-26 16:45:33 +0800

    small updates

--- Assets/ScriptableObjects/GameConstants.cs
+++ Assets/ScriptableObjects/GameConstants.cs
@@ -12,25 +12,6 @@ public  class GameConstants : ScriptableObject
     public float dashDistance = 70;
     public int bombThrowForce = 30;
 
-    // for Reset values
-    Vector3 goombaSpawnPointStart = new Vector3(2.5f, -0.45f, 0); // hardcoded location
-    // .. other reset values 
-
-    // for Consume.cs
-    public  int consumeTimeStep =  10;
-    public  int consumeLargestScale =  4;
-    
-    // for Break.cs
-    public  int breakTimeStep =  30;
-    public  int breakDebrisTorque =  10;
-    public  int breakDebrisForce =  10;
-    
-    // for SpawnDebris.cs
-    public  int spawnNumberOfDebris =  10;
-    
-    // for Rotator.cs
-    public  int rotatorRotateSpeed =  6;
-    
-    // for testing
-    public  int testValue;
+    // timer
+    public int roundDuration = 300;
 }
