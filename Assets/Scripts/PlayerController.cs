1093e3ce30ddfca278c5b4211e00fc7f62f0fd9a
Sorahawk <sorahawktech@gmail.com>
2022-08-12 10:44:34 +0800

    Merging power-ups into main branch (#10)
    * power up prefabs created and detectable by player
    
    * added speed and confusion IEnumerator
    
    * minor debug
    
    * added power throw and shield
    
    * 3s dash countdown in player controller
    
    * minor debug
    
    * more minor debugs
    
    Co-authored-by: Yi Xian <yyx543@gmail.com>

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -44,7 +44,10 @@ public class PlayerController : CommonController {
     private int bombThrowForce;
 
     private float playerCurrentSpeed;
+    private bool isDash = true;
     private bool inSand = false;
+    private bool powerThrow = false;
+    private bool isShield = false;
 
     // hat
     private int hatIndex = -1;
@@ -81,8 +84,10 @@ public class PlayerController : CommonController {
 
     // automatic callback when corresponding input is detected
     private void OnDash() {
-        if (!isAiming && !carriedBomb){
+        if (!isAiming && !carriedBomb && isDash){
             dashActivated = true;
+            isDash = false;
+            StartCoroutine(DashReset());
         }
     }
 
@@ -139,9 +144,16 @@ public class PlayerController : CommonController {
             bombScript.ActivateBomb();
 
             // normalize direction vector and throw bomb
-            latestDir.y = 0.1f;
-            bombBody.AddForce(latestDir / latestDir.magnitude * bombThrowForce, ForceMode.Impulse);
+            if (powerThrow) {
+                print("throwing over wall");
+                latestDir.y = 0.2f;  // need to test angle to throw over walls
+                powerThrow = false;
+            } else {
+                latestDir.y = 0.1f;
+            }
 
+            bombBody.AddForce(latestDir / latestDir.magnitude * bombThrowForce, ForceMode.Impulse);
+            
             // play throw animation
             playerAnimator.SetTrigger("bombThrow");
             playerAnimator.SetBool("holdingBomb", false);
@@ -150,6 +162,30 @@ public class PlayerController : CommonController {
 
     // automatic callback when corresponding input is detected
     private void OnUsePowerup() {
+        print(playerVariable.Powerup);
+        if (playerVariable.Powerup==1) {
+            // confusion (call a script that input current player index)
+            print("confusion");
+            StartCoroutine(Confuse((int)playerInput.user.id));
+        } else if (playerVariable.Powerup==2) {
+            // Power throw
+            print("power throw");
+            powerThrow = true;
+        } else if (playerVariable.Powerup==3) {
+            // Shield
+            print("shield");
+            isShield = true;
+        } else if (playerVariable.Powerup==4) {
+            // Speed
+            print("speed");
+            StartCoroutine(SpeedPowerup());
+        } else if (playerVariable.Powerup==5) {
+            // Trap
+            print("trap");
+            // instantiate bear trap prefab at current player position
+            // tag bear trap to player index
+        }
+        playerVariable.SetPowerup(0);
     }
 
     public void SetCarryBomb(GameObject bombObject) {
@@ -191,12 +227,10 @@ public class PlayerController : CommonController {
 
     private void FixedUpdate() {
         if (inSand){
-            playerCurrentSpeed = gameConstants.playerMoveSpeed/2;
+            playerCurrentSpeed = playerVariable.MoveSpeed/2;
         }else if (!inSand){
-            playerCurrentSpeed = gameConstants.playerMoveSpeed;
+            playerCurrentSpeed = playerVariable.MoveSpeed;
         }
-        playerVariable.SetMoveSpeed(playerCurrentSpeed);
-        
 
         // move
         Vector3 movementTranslation = new Vector3(moveVal.x, 0, moveVal.y);
@@ -204,7 +238,7 @@ public class PlayerController : CommonController {
         if (movementTranslation == Vector3.zero) playerAnimator.SetBool("isRunning", false);
         else playerAnimator.SetBool("isRunning", true);
 
-        playerBody.AddForce(movementTranslation * playerVariable.MoveSpeed, ForceMode.Impulse);
+        playerBody.AddForce(movementTranslation * playerCurrentSpeed, ForceMode.Impulse);
 
         // dash
         if (dashActivated) {
@@ -214,7 +248,7 @@ public class PlayerController : CommonController {
 
             // if character is moving, apply dash in direction of movement
             if (movementTranslation != Vector3.zero) dashForce = movementTranslation;
-
+            
             // if character is stationary, apply dash in direction that player is facing
             else dashForce = latestDir;
 
@@ -259,6 +293,12 @@ public class PlayerController : CommonController {
             // playerVariable.SetMoveSpeed(gameConstants.playerMoveSpeed/2);
             inSand = true;
         }
+
+        else if (other.gameObject.CompareTag("Powerup")) {
+            playerVariable.SetPowerup(other.gameObject.GetComponent<Powerup>().powerup_id);
+            Destroy(other.gameObject);
+        }
+
         else if (other.gameObject.CompareTag("BearTrap")) {
             StunPlayer();
         }
@@ -289,6 +329,53 @@ public class PlayerController : CommonController {
 
         // re-enable controls
         playerInput.ActivateInput();
+    }
+
+    // Resetting Dash after 3s
+    private IEnumerator DashReset() {
+        yield return new WaitForSeconds(3);
+        isDash = true;
+    }
+
+    //Speed Boost Powerup
+    private IEnumerator SpeedPowerup() {
+        playerVariable.SetMoveSpeed(gameConstants.playerMoveSpeed*2);
+        yield return new WaitForSeconds(5);
+        playerVariable.SetMoveSpeed(gameConstants.playerMoveSpeed);
+    }
+
+    // Confusion Powerup
+    private IEnumerator Confuse(int playerIndex) {
+        int i = 0;
+        foreach (PlayerVariable var in playerVarList) {
+            if (i!=playerIndex) {
+                var.SetMoveSpeed(var.MoveSpeed*-1);
+            }
+            i++;
+        }
+        yield return new WaitForSeconds(5);
+        i = 0;
+        foreach (PlayerVariable var in playerVarList) {
+            if (i!=playerIndex) {
+                var.SetMoveSpeed(var.MoveSpeed*-1);
+            }
+            i++;
+        }
+    }
+
+    // Shield Powerup
+    public bool CheckShield() {
+        return isShield;
+    }
+
+    public void DisableShield() {
+        StartCoroutine(DisablingShield());
+    }
+
+    // delayed disable for shield by 0.5s
+    private IEnumerator DisablingShield() {
+        yield return new WaitForSeconds(0.5f);
+        isShield = false;
     }
 
     private void DisableHats() {
