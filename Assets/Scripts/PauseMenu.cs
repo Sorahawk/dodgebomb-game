a6c88e6698c60269ab92bf60b5a48820f5751a93
Sorahawk <sorahawktech@gmail.com>
2022-08-15 00:53:47 +0800

    refined HUD code and set up prefabs

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -6,74 +6,49 @@ using UnityEngine.SceneManagement;
 
 
 public class PauseMenu : MonoBehaviour {
-    public BoolVariable pausedBoolVariable;
     public GameObject pauseMenu; 
-    public BoolVariable QuitorRestartBooleanVariable;
-    // public GameObject roundManagerObject;
-    // public PlayerInput playerInput;
-    
-    // private RoundManager roundManager;
+    public GameObject HUD;
 
-    void Start() {
-        // roundManager = roundManagerObject.GetComponent<RoundManager>();
-    }
+    public BoolVariable pausedBoolVariable;
+    public BoolVariable QuitorRestartBooleanVariable;
 
     void Update() {
+        pauseMenu.SetActive(pausedBoolVariable.Value);
+        HUD.SetActive(!pausedBoolVariable.Value);
 
-        if (pausedBoolVariable.Value) {
-            pauseMenu.SetActive(pausedBoolVariable.Value);
-            Time.timeScale = 0;
-        }
-
-        else{
-            Time.timeScale = 1;
-            pauseMenu.SetActive(pausedBoolVariable.Value);
-        }
-        
-        // playerInput = GetComponent<PlayerInput>();
-        // if (playerInput.actions["Pause"].IsPressed()) {
-        //     PauseUnpause();
-        // }
+        if (pausedBoolVariable.Value) Time.timeScale = 0;
+        else Time.timeScale = 1;
     }
-
-    // public static void PauseUnpause()
-    // {
-    //     if (!pauseMenu.activeInHierarchy)
-    //     {
-    //         pauseMenu.SetActive(true);
-    //         Time.timeScale = 0f;
-
-    //         EventSystem.current.SetSelectedGameObject(null);
-    //         EventSystem.curremt.SetSelectedGameObject(resumeButton);
-    //     } else {
-    //         pauseMenu.SetActive(false);
-    //         Time.timeScale =1f;
-            
-    //     }
-    // }
 
     public void RestartButtonPressed(){
         Debug.Log("restart pressed");
-        // roundManager.ResetAllRoundInfo();
+
         Time.timeScale = 1;
+
         pausedBoolVariable.SetValue(false);
         QuitorRestartBooleanVariable.SetValue(true);
+
         SceneManager.LoadScene("Lobby");
     }
 
     public void ResumeButtonPressed(){
         Debug.Log("resume pressed");
+
         Time.timeScale = 1;
+
         pausedBoolVariable.SetValue(false);
+
         pauseMenu.SetActive(false);
     }
 
     public void QuitButtonPressed(){
         Debug.Log("quit pressed");
-        // roundManager.ResetAllRoundInfo();
+
         Time.timeScale = 1;
+
         pausedBoolVariable.SetValue(false);
         QuitorRestartBooleanVariable.SetValue(true);
+
         SceneManager.LoadScene("Start");    
     }
 }
